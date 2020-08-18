959a5289d263651ab73c600ef99ccd9948a56d5b
Manish Gautam <45124835+ManishGautam001@users.noreply.github.com>
2020-08-18 18:48:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('price_predictor_model.pickle', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def calc():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Home price should be $ {}'.format(output))
+
+
+
+
+
+app.run(debug=True,port=1000)    
